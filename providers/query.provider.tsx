dfa5004c11263c3dbf11587784d49fa64744c6c5
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2026-01-20 12:18:08 +0000

    ajusta

--- providers/query.provider.tsx
+++ providers/query.provider.tsx
@@ -9,8 +9,11 @@ export function QueryProvider({ children }: { children: ReactNode }) {
 			new QueryClient({
 				defaultOptions: {
 					queries: {
-						staleTime: 60 * 10000, // 10 minutes
 						refetchOnWindowFocus: false,
+						refetchOnReconnect: false,
+						retry: 1,
+						staleTime: 1000 * 60 * 10,
+						gcTime: 1000 * 60 * 60,
 					},
 				},
 			}),
